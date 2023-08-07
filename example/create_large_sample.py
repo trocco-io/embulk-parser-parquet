97e89b465e7d3a5ec04d33589c6d921166b1f3e5
chikamura <mchikamura01@gmail.com>
2023-08-07 14:10:19 +0900

    fix script. enable customize column

--- example/create_large_sample.py
+++ example/create_large_sample.py
@@ -16,52 +16,68 @@ import time
 import sys
 import os
 import csv
+import json
 
 size = int(sys.argv[1])
 filepath = sys.argv[2]
 _, ext = os.path.splitext(filepath)
 
-integers = [random.randint(0, 1000000) for _ in range(size)]
-floats = [random.random() for _ in range(size)]
-strings = [str(uuid.uuid4()) for _ in range(size)]
-
 def str_time_prop(start, end, time_format, prop):
-    """Get a time at a proportion of a range of two formatted times.
-
-    start and end should be strings specifying times formatted in the
-    given format (strftime-style), giving an interval [start, end].
-    prop specifies how a proportion of the interval to be taken after
-    start.  The returned time will be in the specified format.
-    """
-
     stime = time.mktime(time.strptime(start, time_format))
     etime = time.mktime(time.strptime(end, time_format))
-
     ptime = stime + prop * (etime - stime)
-
     return time.strftime(time_format, time.localtime(ptime))
 
 
 def random_date(start, end, prop):
     return str_time_prop(start, end, "%Y-%m-%dT%H:%M:%S", prop)
 
-timestamps = [random_date("1970-01-01T00:00:00", "2020-01-01T00:00:00", random.random()) for _ in range(size)]
+def create_random_integers(size):
+    return [random.randint(0, 1000000) for _ in range(size)]
+
+def create_random_floats(size):
+    return [random.random() for _ in range(size)]
+
+def create_random_strings(size):
+    return [str(uuid.uuid4()) for _ in range(size)]
+
+def create_random_timestamps(size):
+    return [random_date("1970-01-01T00:00:00", "2020-01-01T00:00:00", random.random()) for _ in range(size)]
+
+if len(sys.argv) >= 4:
+    columns = json.loads(sys.argv[3])
+else:
+    columns = { "integer" : "integer", "float" : "float", "string" : "string", "timestamp" : "timestamp" }
+
+data = {}
+
+for name, column_type in columns.items():
+    if column_type == "integer":
+        data[name] = create_random_integers(size)
+    elif column_type == "float":
+        data[name] = create_random_floats(size)
+    elif column_type == "string":
+        data[name] = create_random_strings(size)
+    elif column_type == "timestamp":
+        data[name] = create_random_timestamps(size)
+    else:
+        raise 'invalid column type'
 
 if ext == ".parquet":
-    df = pd.DataFrame({
-        "integer": integers,
-        "float": floats,
-        "string": strings,
-        "timestamp": timestamps
-    })
+    df = pd.DataFrame(data)
     print(df.info())
     df.to_parquet(filepath)
 elif ext == ".csv":
     with open(filepath, "w") as f:
         writer = csv.writer(f)
-        writer.writerow(["integer", "float", "string", "timestamp"])
+        names = data.keys()
+        writer.writerow(names)
         for i in range(size):
-            writer.writerow([integers[i], floats[i], strings[i], timestamps[i]])
+            row = []
+            for name in names:
+                row.append(data[name][i])
+
+            writer.writerow(row)
 else:
     raise 'invalid extension. ext must be .csv or .parquet.'
 
