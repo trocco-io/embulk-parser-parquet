21eb37bad5635e7b572fdd29bbd1225f0c374a4c
chikamura <mchikamura01@gmail.com>
2023-09-24 18:37:04 +0900

    fix and refactoring parquetFile validation

--- src/main/java/org/embulk/parquet/ParquetUtil.java
+++ src/main/java/org/embulk/parquet/ParquetUtil.java
@@ -3,6 +3,8 @@ package org.embulk.parquet;
 import java.io.*;
 import java.nio.file.Files;
 import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
@@ -11,13 +13,13 @@ public class ParquetUtil {
     private static final byte[] PARQUET_MAGIC_NUMBER = {0x50, 0x41, 0x52, 0x31};
 
     public static boolean isParquetFile(byte[] bytes) {
-        if (PARQUET_MAGIC_NUMBER.length > bytes.length) {
+        if (PARQUET_MAGIC_NUMBER.length * 2 > bytes.length) {
             return false;
         }
         for (int i = 0; i < PARQUET_MAGIC_NUMBER.length; i++) {
-            if (PARQUET_MAGIC_NUMBER[i] != bytes[i]
-                    || PARQUET_MAGIC_NUMBER[i]
-                            != bytes[bytes.length - PARQUET_MAGIC_NUMBER.length + i]) {
+            byte magicNumber = PARQUET_MAGIC_NUMBER[i];
+            int lastIndex = bytes.length - PARQUET_MAGIC_NUMBER.length + i;
+            if (magicNumber != bytes[i] || magicNumber != bytes[lastIndex]) {
                 return false;
             }
         }
@@ -27,9 +29,8 @@ public class ParquetUtil {
     public static ParquetReader<Object> buildReader(InputStream inputStream) {
         final String path = copyToFile(inputStream).getAbsolutePath();
         AvroParquetReader.Builder<Object> builder = AvroParquetReader.builder(new Path(path));
-        org.apache.hadoop.conf.Configuration configuration =
-                new org.apache.hadoop.conf.Configuration();
-        configuration.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+        Configuration configuration = new Configuration();
+        configuration.set("fs.file.impl", LocalFileSystem.class.getName());
         try {
             return builder.withConf(configuration).build();
         } catch (IOException e) {
